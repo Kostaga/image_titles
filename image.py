34b7ba954450965807db3c302c4750560250b2fb
Konstantinos Agathopoulos <kostaga203@gmail.com>
2023-04-19 17:36:42 +0300

    made new branch for suites "directoryName_number.jpg"

--- image.py
+++ image.py
@@ -5,18 +5,25 @@ rootdir = r"C:\Users\Konstaninos\Downloads\imagecompressor"
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if (re.search("-min",file)):
+        if (re.search("-min", file)):
             newfile = re.sub("-min", "", file)
-            os.rename(f"{subdir}\{file}",f"{subdir}\{newfile}")
-        if (re.search(" ",file)):
+            os.rename(f"{subdir}\{file}", f"{subdir}\{newfile}")
+        if (re.search(" ", file)):
             newfile = re.sub(" ", "_", file)
             os.rename(f"{subdir}\{file}", f"{subdir}\{newfile}")
 
-        if (re.search("[0-9]{3,4}",file)):
-            res = re.search("[0-9]{3,4}",file)
-            period = re.search(".jpg",file)
+        if (re.search(r"[0-9]{3,4}", file)):
+            res = re.search(r"[0-9]{3,4}", file)
+            if (re.search(r".jpg", file)):
+                period = re.search(".jpg", file)
+            else:
+                period = re.search(".JPG", file)
             path = subdir
             path = os.path.basename(path)
-            newfile = f"{path.lower()}_{file[res.start():period.start()]}.jpg"
+            name = os.path.basename(os.path.dirname(subdir))
+            try:
+                newfile = f"{name.lower()}_{file[res.start():period.start()]}.jpg"
+                os.rename(f"{subdir}\{file}", f"{subdir}\{newfile}")
+            except AttributeError:
+                print("oi")
 
-            os.rename(f"{subdir}\{file}", f"{subdir}\{newfile}")
